81ea423240d32f3ac7c9ed949b72e82d6641718c
Nicolas CLERC <nicolas.clerc@sablefin.net>
2017-05-14 17:20:10 -0700

    add project for long file

---
+++ BigAudioClientTestApp/LongRecognitionTester.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Bing.Speech;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BigAudioClientTestApp
+{
+    // NUGET  ADD : Microsoft.Bing.Speech
+
+
+     class LongRecognitionTester
+    {
+        
+        public async Task<string> RecognizeFile(Stream audio)
+        {
+            string authKey = System.Configuration.ConfigurationManager.AppSettings["bingSpeechToTextApiKey"];
+
+            // for url composition, see : https://docs.microsoft.com/en-us/azure/cognitive-services/speech/api-reference-rest/bingvoicerecognition
+            Uri LongDictationUrl = new Uri(@"wss://speech.platform.bing.com/api/service/recognition/continuous");
+
+            var cts = new CancellationTokenSource();
+
+            // create the preferences object
+            var preferences = new Preferences("fr-FR", LongDictationUrl, new CognitiveServicesAuthorizationProvider(authKey));
+
+            List<RecognitionResult> results = new List<RecognitionResult>();
+
+            using (var speechClient = new SpeechClient(preferences))
+            { 
+                speechClient.SubscribeToRecognitionResult(async (rr) =>
+                {
+                    await Task.Run(() =>
+                    {
+                        results.Add(rr);
+                        Console.WriteLine("--------------------------------------------");
+                        Console.WriteLine($"RecognitionStatus = ${rr.RecognitionStatus}");
+                        foreach(var p in rr.Phrases)
+                        {
+                            Console.WriteLine($"  {p.Confidence} {p.DisplayText}");
+                        }
+                    });
+                });
+
+                var deviceMetadata = new DeviceMetadata(DeviceType.Near, 
+                    DeviceFamily.Desktop, 
+                    NetworkType.Unknown, 
+                    OsName.Unknown, "1607", "Azure", "Function");
+                var applicationMetadata = new ApplicationMetadata("ResilientLongVtt", "1.0.0");
+                var requestMetadata = new RequestMetadata(Guid.NewGuid(), deviceMetadata, applicationMetadata, "SampleAppService");
+                
+                 await speechClient.RecognizeAsync(new SpeechInput(audio, requestMetadata), cts.Token);
+
+                Console.Write($"TOTAL RESULTS : {results.Count}");
+                
+            }
+
+            return "rien";
+        }
+
+      
+
+    }
+}
---
+++ BigAudioClientTestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigAudioClientTestApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string filename;
+            //filename= @"c:\record\tardis_nc-voixderoutante-PCM16.wav";
+            filename = @"c:\record\tardis-nc.wav";
+            //filename = @"c:\record\audio_1_mono_16kpcm.wav";
+            using (var audioStream = File.OpenRead(filename))
+            {
+                var t = new LongRecognitionTester();
+                var result = t.RecognizeFile(audioStream).GetAwaiter().GetResult();
+                Console.WriteLine($"RECOGNITION RESULT : {result}");
+            }
+            Console.ReadLine();
+        }
+      
+    }
+}
